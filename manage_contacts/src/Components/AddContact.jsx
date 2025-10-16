396ffe164869c5251f01cfa8f264e69b2312f92b
vibhanimbalkar97-star <vibhanimbalkar97@gmail.com>
2025-10-16 11:59:42 +0530

    Addcontact function created
    form created, onchange and addcontact functions created

--- manage_contacts/src/Components/AddContact.jsx
+++ manage_contacts/src/Components/AddContact.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddContact = () => {
+const [input, setInput] = useState({name: "", email: ""})
+
+const handleOnChange= (e) => {
+   if(e.target.name === "name"){
+    setInput({...input, name: e.target.value});
+   } else {
+    setInput({...input, email:e.target.value})
+   }
+        setInput("")
+
+}
+
+const handleAdd = () => {
+    if(input.name === "" || input.email === ""){
+        alert("Fill all the details");
+        return;
+    }
+    console.log(input)
+}
+
 
     return (
         <div>
@@ -8,14 +28,15 @@ const AddContact = () => {
             <form>
                 <label>Name:</label>
                 <br />
-                <input type="text" placeholder="Enter name" />
+                <input type="text" placeholder="Enter name" name="name" value={input.name} onChange={handleOnChange}/>
                 <br /><br />
                 <label>Email:</label>
                 <br />
-                <input type="text" placeholder="Enter Email" />
+                <input type="text" placeholder="Enter Email" name="email" value={input.email} onChange={handleOnChange}/>
                 <br /><br />
-                <button>Add Contact</button>
+               
             </form>
+             <button onClick={handleAdd}>Add Contact</button>
         </div>
     )
 }
